2e86aedb594639c0b1916b5ad0871c14ac65cc78
pl4ko <s1910237019@students.fh-hagenberg.at>
2020-05-18 17:05:36 +0200

    neuesWertpapier

--- app/src/main/java/com/example/wpgeschaefte/neuesWertpapier.kt
+++ app/src/main/java/com/example/wpgeschaefte/neuesWertpapier.kt
@@ -1,5 +1,6 @@
 package com.example.wpgeschaefte
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,26 +8,26 @@ import android.view.Menu
 import kotlinx.android.synthetic.main.activity_homescreen.*
 import kotlinx.android.synthetic.main.activity_neues_wertpapier.*
 
-class neuesWertpapier : AppCompatActivity() {
+class neuesWertpapier : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_neues_wertpapier)
 
-        setSupportActionBar(toolbar_neues_wertpapier)
+/*   setSupportActionBar(toolbar_neues_wertpapier)
 
-        bT_home.setOnClickListener{
-            intent =  Intent(this, HomeScreen::class.java)
-            startActivity(this.intent)
-        }
+   bT_home.setOnClickListener{
+       intent =  Intent(this, HomeScreen::class.java)
+       startActivity(this.intent)
+   }
 
-        bT_cancel.setOnClickListener{
-            intent = Intent(this,HomeScreen::class.java)
-            startActivity(intent)
-        }
-
-    }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_neueswertpapier, menu)
-        return true
-    }
+   bT_cancel.setOnClickListener{
+       intent = Intent(this,HomeScreen::class.java)
+       startActivity(intent)
+   }
+*/
+}
+override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+   menuInflater.inflate(R.menu.menu_neueswertpapier, menu)
+   return true
+}
 }
