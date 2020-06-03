cb041d7c09d0f25324958278ed0384023d53f632
MarioEberth <marioeberth@gmail.com>
2020-06-03 14:58:14 +0200

    add recycler in home screen

--- app/src/main/java/com/example/wpgeschaefte/HomeScreen.kt
+++ app/src/main/java/com/example/wpgeschaefte/HomeScreen.kt
@@ -2,13 +2,17 @@ package com.example.wpgeschaefte
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_homescreen.*
 
 class HomeScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homescreen)
@@ -20,6 +24,14 @@ class HomeScreen : AppCompatActivity() {
             R.layout.support_simple_spinner_dropdown_item,
             resources.getStringArray(R.array.sP_list_homescreen)
         )
+
+        val list = arrayListOf<Aktie>()
+        list.add(Aktie("OMV", "austria"))
+        list.add(Aktie("Voest", "Italien"))
+
+
+        rV_aktien.layoutManager = LinearLayoutManager(this)
+        rV_aktien.adapter = MyRecyclerAdapter(list)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -37,3 +49,50 @@ class HomeScreen : AppCompatActivity() {
         }
     }
 }
+
+//----------------------------------------------------------------
+//Recycler Stuff
+class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    val tV_name = view.findViewById<TextView>(R.id.home_recycleritem_Name)
+
+    init {
+
+    }
+}
+
+class MyRecyclerAdapter(val list: MutableList<Aktie>) : RecyclerView.Adapter<MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.home_recycleritem,
+            parent,
+            false
+        )
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val item = list[position]
+        holder.tV_name.text = item.name
+
+        //get a Toast message with the the country text > if you clicked on the item
+        holder.itemView.setOnClickListener{
+            Toast.makeText(
+                it.context,"${item.name} , ${item.symbol}", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+}
+
+//---------------------------------------------------
+//data stuff
+
+data class Aktie(val name: String, val symbol: String)
